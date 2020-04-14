c5fe5de8f20a53cf4bc5717c75eedc8f081428de
chryen <squirtlexomg@yahoo.com>
2020-04-14 15:36:23 -0500

    Added express router and and required api

--- server/index.js
+++ server/index.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const path = require('path');
+const { apiRouter } = require('./api');
 
 const app = express();
 
 const PORT = 8080;
-
 const CLIENT_PATH = path.join(__dirname, '../client/dist');
 
 app.use(express.static(CLIENT_PATH));
-
-
+app.use(express.json());
+app.use('/api', apiRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on :${PORT} 🚀`);
 });
+
+module.exports = {
+  app,
+};
