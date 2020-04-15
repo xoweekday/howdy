0f58ccec4ed665aeb9f38d8bc370f61657d5bb19
hartsean <hartsean@gmail.com>
2020-04-15 15:07:54 -0500

    list item rendering inline with dummy-details

--- client/src/components/PartyCreation/PartyListItem.jsx
+++ client/src/components/PartyCreation/PartyListItem.jsx
@@ -4,13 +4,28 @@ class PartyListItem extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      parties: [],
     };
   }
 
+  //Render individual party details. 
   render() {
     return (
       <div>
-        <h4>Party List Item</h4>
+        {/* {parties.map(( party ) => */}
+          <div className='partyContainer' class='container'>
+            <div class="row">
+              <div className='partyName' class='col'>Cool</div>
+              <div className='partyDistance' class='col'>6miles</div>
+              <div className='partySize' class='col'>3party-goers</div>
+              <div className='partyHost' class='col'>John</div>
+              <div className='partyStartTime' class='col'>8pm</div>
+              <div className='partyEndTime' class='col'>10pm</div>
+              <div className='partyLink' class='col'>link to party</div>
+            </div>
+          </div>
+        {/* )} */}
+
       </div>
     );
   }
