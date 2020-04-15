e5d001054139c1539b507887ff64839a8e7d7a5a
Heather Haylett <heatherhaylett@gmail.com>
2020-04-15 11:44:30 -0500

    (feat) added signupRouter

--- server/api/index.js
+++ server/api/index.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
 const { homeRouter } = require('./homepage');
+const { signupRouter } = require('./signup');
 
 const apiRouter = Router();
 
 apiRouter.use('/homepage', homeRouter);
+apiRouter.use('/signup', signupRouter);
 
 module.exports = {
   apiRouter,
