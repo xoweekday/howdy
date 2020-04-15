508db0d71b03d7b4e3d774881d57f6b7410aadd5
hartsean <hartsean@gmail.com>
2020-04-15 15:57:51 -0500

    removed comment

--- client/src/components/PartyCreation/PartyListItem.jsx
+++ client/src/components/PartyCreation/PartyListItem.jsx
@@ -12,19 +12,19 @@ class PartyListItem extends React.Component {
   render() {
     return (
       <div>
-        {/* {parties.map(( party ) => */}
-          <div className='partyContainer' class='container'>
-            <div class="row">
-              <div className='partyName' class='col'>Cool</div>
-              <div className='partyDistance' class='col'>6miles</div>
-              <div className='partySize' class='col'>3party-goers</div>
-              <div className='partyHost' class='col'>John</div>
-              <div className='partyStartTime' class='col'>8pm</div>
-              <div className='partyEndTime' class='col'>10pm</div>
-              <div className='partyLink' class='col'>link to party</div>
-            </div>
+
+        <div className='partyContainer' class='container'>
+          <div class="row">
+            <div className='partyName' class='col'>Cool Party Name</div>
+            <div className='partyDistance' class='col'>6miles</div>
+            <div className='partySize' class='col'>3party-goers</div>
+            <div className='partyHost' class='col'>John</div>
+            <div className='partyStartTime' class='col'>8pm</div>
+            <div className='partyEndTime' class='col'>10pm</div>
+            <div className='partyLink' class='col'>link to party</div>
           </div>
-        {/* )} */}
+        </div>
+
 
       </div>
     );
