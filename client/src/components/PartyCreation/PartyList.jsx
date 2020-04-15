23ea0a987abaf9a7f3c8f6ec47f2f9e6a25ccc18
hartsean <hartsean@gmail.com>
2020-04-15 15:55:11 -0500

    added getParties function with axios request to DB for party data

--- client/src/components/PartyCreation/PartyList.jsx
+++ client/src/components/PartyCreation/PartyList.jsx
@@ -7,17 +7,32 @@ class PartyList extends React.Component {
     super(props);
     this.state = {
       items: [],
-      parties: [{},{},{}],
+      parties: [],
     };
   }
 
+  //Get Parties from DB 
+  getParties() {
+    axios.get('/api/phrases')
+    .then(response => this.setState({parties: response.data}))
+    .catch(error => {throw error})
+  }
+
+  //Get Distance Function
+  getDistance(userInfo) {
+    //take the user's location, and the location of each party in the database. 
+    //if the user's location is within the specified range, show the party details and the distance between user and party. 
+  
+  }
+
+  //we only render parties that are in the user's range. 
   render() {
     return (
       <div>
         <h3>Party List</h3>
-        {Parties.map(( party ) =>
-          <PartyListItem ></PartyListItem>
-        )}
+        {/* {parties.map(( party ) => */}
+        <PartyListItem ></PartyListItem>
+        {/* )} */}
       </div>
     );
   }
