657c0f3d0068471e1ea716239785a6e29acfbe0d
Heather Haylett <heatherhaylett@gmail.com>
2020-04-15 11:46:33 -0500

    (feat) added loginRouter

--- server/api/index.js
+++ server/api/index.js
@@ -1,11 +1,13 @@
 const { Router } = require('express');
 const { homeRouter } = require('./homepage');
 const { signupRouter } = require('./signup');
+const { loginRouter } = require('./login');
 
 const apiRouter = Router();
 
 apiRouter.use('/homepage', homeRouter);
 apiRouter.use('/signup', signupRouter);
+apiRouter.use('/login', loginRouter);
 
 module.exports = {
   apiRouter,
