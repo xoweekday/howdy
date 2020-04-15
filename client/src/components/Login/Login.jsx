113fdf47be6ac109ec8d69678cbd20650f7aaf26
matthritz <atmatthritz@gmail.com>
2020-04-15 16:39:48 -0500

    CreateParty input placeholders made

---
+++ client/src/components/Login/Login.jsx
@@ -0,0 +1,10 @@
+const LogIn = () => (
+  <div>
+    <input type="text" name="userName" id="userName"/>
+  </div>
+  <div>
+    <input type="submit" value=""/>
+  </div>
+)
+
+export default LogIn;
--- client/src/components/PartyCreation/CreateParty.jsx
+++ client/src/components/PartyCreation/CreateParty.jsx
@@ -11,6 +11,24 @@ class CreateParty extends React.Component {
     return (
       <div>
         <h3>Party Creation Tools</h3>
+          <div> Party
+            <input type="text" partyName="name" placeholder="Name Your Party" />
+          </div>
+          <div> Start Time
+            <input type="time" name="startTime" id="startTime"/>
+          </div>
+          <div> End Time
+            <input type="time" name="endTime" id="endTime"/>
+          </div>
+          <div> Date
+            <input type="date" name="date" id="date"/>
+          </div>
+          <div> Range
+            <input type="number" name="range" id="range" placeholder='Miles'/>
+          </div>
+          <div> Make A Party
+            <input type="submit" value="submit"/>
+          </div>
       </div>
     );
   }
