b966f5ba85b8f9323f230d6f286e6ba34e5df749
matthritz <atmatthritz@gmail.com>
2020-04-16 16:30:17 -0500

    some form alignment styling for Create Parties

--- client/src/components/PartyCreation/CreateParty.jsx
+++ client/src/components/PartyCreation/CreateParty.jsx
@@ -32,62 +32,84 @@ class CreateParty extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="container">
+        <div className="row">
+          <div className="col center">
         <form onSubmit={this.handleSubmit}>
           <h3>Party Creation Tools</h3>
+      <div class="form-group">
           <label>
             Party
             <input
+              class="form-control"
               name="name"
               type="text"
               value={this.state.name}
               placeholder="Name Your Party"
               onChange={this.handleChange}
-            />
+              />
           </label>
+            </div>
+            <div class="form-group">
           <label>
             Start Time
             <input
+              class="form-control"
               name="start"
               type="time"
               value={this.state.start}
               onChange={this.handleChange}
-            />
+              />
           </label>
+          </div>
+          <div class="form-group">
           <label>
             End Time
             <input
+              class="form-control"
               type="time"
               name="end"
               onChange={this.handleChange}
-            />
+              />
           </label>
+          </div>
+          <div class="form-group">
           <label>
             Date
             <input
+              class="form-control"
               type="date"
               name="date"
               onChange={this.handleChange}
-            />
+              />
           </label>
+          </div>
+          <div class="form-group">
           <label>
             Radius
             <input
+              class="form-control"
               type="number"
               name="radius"
               placeholder="Miles"
               onChange={this.handleChange}
-            />
+              />
           </label>
-          <label> 
-            Make A Party
+          </div>
+          <div class="form-group">
+          <label>
             <input
+              class="form-control"
               type="submit"
               value="submit"
-            />
+              class="btn btn-primary"
+              />
           </label>
+          </div>
         </form>
       </div>
+              </div>
+            </div>
     );
   }
 }
