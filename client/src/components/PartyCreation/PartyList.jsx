f232cc615d71e1f97f94af81825d0d763262cfc1
hartsean <hartsean@gmail.com>
2020-04-16 11:21:33 -0500

    linting fixes

--- client/src/components/PartyCreation/PartyList.jsx
+++ client/src/components/PartyCreation/PartyList.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import PartyListItem from './PartyListItem.jsx';
 import axios from 'axios';
+import PartyListItem from './PartyListItem.jsx';
+
 class PartyList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: [],
       parties: [],
     };
   }
@@ -14,39 +14,31 @@ class PartyList extends React.Component {
     this.getParties();
   }
 
-  //Get Parties from DB 
+  //  Get Parties from DB
   getParties() {
     axios.get('/api/homepage')
-      .then(response => this.setState({ parties: response.data }))
-      .catch(error => { throw error })
+      .then((response) => this.setState({ parties: response.data }))
+      .catch((error) => { throw error; });
   }
 
-  //Get Distance Function
-  getDistance(userInfo) {
-    //take the user's location, and the location of each party in the database. 
-    //if the user's location is within the specified range, show the party details and the distance between user and party. 
-
-  }
-
-  //we only render parties that are in the user's range. 
+  //  we only render parties that are in the user"s range.
   render() {
+    const { parties } = this.state;
     return (
       <div>
         <h1>Parties near you!</h1>
-        <div className='partyContainer' className='container'>
+        <div className="partyContainer container">
           <div className="row">
-            <div className='col'><h4>Party Name</h4></div>
-            <div className='col'><h4>Location</h4></div>
-            <div className='col'><h4>Start Time</h4></div>
-            <div className='col'><h4>Party Details</h4></div>
-            <div className='col'><h4>Party Details</h4></div>
-            <div className='col'><h4>Party Details</h4></div>
-            <div className='col'><h4>Link to Join</h4></div>
+            <div className="col"><h4>Party Name</h4></div>
+            <div className="col"><h4>Location</h4></div>
+            <div className="col"><h4>Start Time</h4></div>
+            <div className="col"><h4>Party Details</h4></div>
+            <div className="col"><h4>Party Details</h4></div>
+            <div className="col"><h4>Party Details</h4></div>
+            <div className="col"><h4>Link to Join</h4></div>
           </div>
         </div>
-        {this.state.parties.map((party) =>
-          <PartyListItem key={party.id} party={party} ></PartyListItem>
-        )}
+        {parties.map((party) => <PartyListItem key={party.id} party={party} />)}
       </div>
     );
   }
