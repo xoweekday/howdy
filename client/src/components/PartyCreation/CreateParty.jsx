180735cfd3199097dcf3466ba951bf0abd8f3b4f
matthritz <atmatthritz@gmail.com>
2020-04-16 12:00:19 -0500

    axios post request from CreatParty form functioning

--- client/src/components/PartyCreation/CreateParty.jsx
+++ client/src/components/PartyCreation/CreateParty.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 class CreateParty extends React.Component {
   constructor(props) {
@@ -14,13 +15,16 @@ class CreateParty extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event){
+  handleChange(event) {
     this.setState({ [event.target.name]: event.target.value });
   }
 
-  handleSubmit(event){
+  handleSubmit(event) {
     event.preventDefault();
-    console.log('clicked the submit button: ', this.state)
+    console.log('clicked the submit button: ', this.state);
+    axios.post('/api/homepage', this.state)
+      .then((response) => { console.log(response); })
+      .catch((error) => { console.log(error); });
   }
 
   render() {
@@ -28,52 +32,58 @@ class CreateParty extends React.Component {
       <div>
         <form onSubmit={this.handleSubmit}>
           <h3>Party Creation Tools</h3>
-            <label> Party
-              <input
-                name="name"
-                type="text"
-                value={this.state.name}
-                placeholder="Name Your Party"
-                onChange={this.handleChange}
-              />
-            </label>
-            <label> Start Time
-              <input
-                name="start"
-                type="time"
-                value={this.state.start}
-                onChange={this.handleChange}
-              />
-            </label>
-            <label> End Time
-              <input
-                type="time"
-                name="end"
-                onChange={this.handleChange}
-              />
-            </label>
-            <label> Date
-              <input
-                type="date"
-                name="date"
-                onChange={this.handleChange}
-              />
-            </label>
-            <label> Range
-              <input
-                type="number"
-                name="range"
-                placeholder='Miles'
-                onChange={this.handleChange}
-              />
-            </label>
-            <label> Make A Party
-              <input
-                type="submit"
-                value="submit"
-              />
-            </label>
-          </form>
+          <label>
+            Party
+            <input
+              name="name"
+              type="text"
+              value={this.state.name}
+              placeholder="Name Your Party"
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Start Time
+            <input
+              name="start"
+              type="time"
+              value={this.state.start}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            End Time
+            <input
+              type="time"
+              name="end"
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Date
+            <input
+              type="date"
+              name="date"
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Range
+            <input
+              type="number"
+              name="range"
+              placeholder="Miles"
+              onChange={this.handleChange}
+            />
+          </label>
+          <label> 
+            Make A Party
+            <input
+              type="submit"
+              value="submit"
+            />
+          </label>
+        </form>
       </div>
     );
   }
