50b2acef1abfe3be4d01a9f2fc1ba10ac142983e
James Easter <james.m.easter@gmail.com>
2020-04-17 16:09:16 -0500

    clear out extra comments

--- client/src/components/PartyCreation/PartyList.jsx
+++ client/src/components/PartyCreation/PartyList.jsx
@@ -1,50 +1,7 @@
 import React from 'react';
-// import axios from 'axios';
 import PartyListItem from './PartyListItem.jsx';
 
-// class PartyList extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       parties: [],
-//     };
-//   }
-
-//   componentDidMount() {
-//     this.getParties();
-//   }
-
-//   //  Get Parties from DB
-//   getParties() {
-//     axios.get('/api/homepage')
-//       .then((response) => this.setState({ parties: response.data }))
-//       .catch((error) => { throw error; });
-//   }
-
-//   //  we only render parties that are in the user"s range.
-//   render() {
-//     const { parties } = this.state;
-//     return (
-//       <div>
-//         <h1>Parties near you!</h1>
-//         <div className="partyContainer container">
-//           <div className="row">
-//             <div className="col"><h4>Party Name</h4></div>
-//             <div className="col"><h4>Location</h4></div>
-//             <div className="col"><h4>Start Time</h4></div>
-//             <div className="col"><h4>Party Details</h4></div>
-//             <div className="col"><h4>Party Details</h4></div>
-//             <div className="col"><h4>Party Details</h4></div>
-//             <div className="col"><h4>Link to Join</h4></div>
-//           </div>
-//         </div>
-//         {parties.map((party) => <PartyListItem key={party.id} party={party} />)}
-//       </div>
-//     );
-//   }
-// }
-
-const PartyList = ({ parties }) => (
+const PartyList = ({ parties, getPartyInfo }) => (
     <div>
       <h1>Parties near you!</h1>
       <div className="partyContainer container">
@@ -58,7 +15,7 @@ const PartyList = ({ parties }) => (
           <div className="col"><h4>Link to Join</h4></div>
         </div>
       </div>
-      {parties.map((party) => <PartyListItem key={party.id} party={party} />)}
+      {parties.map((party) => <PartyListItem key={party.id} party={party} getPartyInfo={getPartyInfo} />)}
     </div>
   );
 
