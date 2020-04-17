0c93ba3b86cb85d65d9d712a9f952851107bfd2b
chryen <squirtlexomg@yahoo.com>
2020-04-17 14:39:04 -0500

    Added get and post for api/login

--- server/api/login.js
+++ server/api/login.js
@@ -3,6 +3,30 @@ const db = require('../db');
 
 const loginRouter = Router();
 
+// POST /api/login
+// when a user signs in through google, add the details to the database
+loginRouter.post('/', (req, res) => {
+  db.addUser(req)
+  .then(() => res.send(201))
+  .catch((error) => {
+    console.log(error);
+    res.sendStatus(500);
+  });
+});
+
+// GET /api/login
+// 
+loginRouter.get('/', (req, res) => {
+  db.getRooms()
+    .then((parties) => {
+      res.send(parties);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.sendStatus(500);
+    });
+});
+
 module.exports = {
   loginRouter,
 };
