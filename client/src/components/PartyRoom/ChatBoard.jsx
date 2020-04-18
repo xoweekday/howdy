2b556bb78eddb3dfa0ae1291a79c4e1e1c3f61ae
James Easter <james.m.easter@gmail.com>
2020-04-17 22:15:31 -0500

    (feat) add party details to ChatBoard

--- client/src/components/PartyRoom/ChatBoard.jsx
+++ client/src/components/PartyRoom/ChatBoard.jsx
@@ -6,6 +6,11 @@ const ChatBoard = ({ messages, partyInfo }) => (
       <h3>Welcome to the {partyInfo.name}</h3>
     </div>
 
+    <div>
+      <h5>Party Details:</h5>
+      <p>{partyInfo.details}</p>
+    </div>
+
     <div className="row">
       <div className="messages-box col-md-12">
         {messages.map((message) =>
