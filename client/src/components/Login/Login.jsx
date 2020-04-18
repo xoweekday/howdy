aeca0de7315bc078f72bf6cfef06b59d717155d6
chryen <squirtlexomg@yahoo.com>
2020-04-18 14:56:41 -0500

    (cleanup) Centered elements and added title

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -35,9 +35,12 @@ class LogIn extends React.Component {
   render() {
     const { name, image_url, view } = this.state;
     return (
-      <div>
+      <div className="loginGoogle">
+        <h1>WELCOME TO HOWDY</h1>
         {/* <div className="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></div> */}
         {view === false ?
+        <div>
+          <h4>Please sign in with Google: </h4>
           <GoogleLogin
             clientId="803513597131-sm1rmc2bobnambuak7u7b75a7kh61g6i.apps.googleusercontent.com"
             buttonText="Login"
@@ -47,22 +50,23 @@ class LogIn extends React.Component {
             cookiePolicy={'single_host_origin'}
             // uxMode="redirect"
             redirectUri="https://localhost:8080/#/parties"
-          /> : null
+          /> 
+        </div> : null
         }
         {view === true ? 
-          <div>
-            <h3>You have signed in as:</h3>
-            <div>{name}</div>
-            <div><img src={image_url}/></div>
-            <Link to={{ pathname: '/parties' }}>
-              <button> Click here to allow your location! </button>  
-            </Link>
-            <div> Or add your zip code! </div>
-            <input placeholder="zip code" />
-            <Link to={{ pathname: '/parties' }}>
-              <button> Submit </button>  
-            </Link>
-          </div> : null
+        <div className="loginSigned" align="center">
+          <h3>You have signed in as:</h3>
+          <div>{name}</div>
+          <div><img src={image_url}/></div>
+          <Link to={{ pathname: '/parties' }}>
+            <button> Click here to allow your location! </button>  
+          </Link>
+          <div> Or enter your zip code! </div>
+          <input placeholder="zip code" />
+          <Link to={{ pathname: '/parties' }}>
+            <button> Submit </button>  
+          </Link>
+        </div> : null
         }
       </div>
     )
