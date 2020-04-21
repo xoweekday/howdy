b9c98cff7c631198d4631c28a06c102e3bc5dee4
James Easter <james.m.easter@gmail.com>
2020-04-21 00:58:27 -0500

    (refactor) moves people list to chat board

--- client/src/components/PartyRoom/ChatRoom.jsx
+++ client/src/components/PartyRoom/ChatRoom.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import ChatBoard from './ChatBoard.jsx';
-import ChatPeopleList from './ChatPeopleList.jsx';
 
 const ChatRoom = ({ partyInfo, username }) => {
   return(
     <div className="container">
       <div className="row">
         <div className="chatRoomContainer col-md-8"><ChatBoard partyInfo={partyInfo} username={username} /></div>
-        <div className="chatPeopleListContainer col-md-4"><ChatPeopleList /></div>
       </div>
     </div>
   );
