850f013548fe74dc35cefc0a5e525fab8e5b009d
James Easter <james.m.easter@gmail.com>
2020-04-20 22:40:11 -0500

    (refactor) move button inside of ChatBoard

--- client/src/components/PartyRoom/ChatRoom.jsx
+++ client/src/components/PartyRoom/ChatRoom.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ChatBoard from './ChatBoard.jsx';
 import ChatPeopleList from './ChatPeopleList.jsx';
-import { Link } from 'react-router-dom';
 
 const ChatRoom = ({ partyInfo, username }) => {
   return(
@@ -9,9 +8,6 @@ const ChatRoom = ({ partyInfo, username }) => {
       <div className="row">
         <div className="chatRoomContainer col-md-8"><ChatBoard partyInfo={partyInfo} username={username} /></div>
         <div className="chatPeopleListContainer col-md-4"><ChatPeopleList /></div>
-        <Link to={{ pathname: '/parties' }}>
-          <button type="button" className="btn btn-primary">Leave Party</button>
-        </Link>
       </div>
     </div>
   );
