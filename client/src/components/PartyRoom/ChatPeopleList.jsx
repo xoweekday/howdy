a7c138ab67b37ba7fcda46e3c74dd3a9732bc1a5
hartsean <hartsean@gmail.com>
2020-04-22 22:03:21 -0500

    Added game rules to CharPeopleList Component

--- client/src/components/PartyRoom/ChatPeopleList.jsx
+++ client/src/components/PartyRoom/ChatPeopleList.jsx
@@ -2,11 +2,11 @@ import React, { useState } from 'react';
 
 const ChatPeopleList = ({ users, username }) => {
 
-  const [ flag, setFlag ] = useState(false);
+  const [flag, setFlag] = useState(false);
 
   const showCharacter = (e) => {
     e.preventDefault();
-    if(!flag){
+    if (!flag) {
       setFlag(true);
     } else {
       setFlag(false);
@@ -16,12 +16,15 @@ const ChatPeopleList = ({ users, username }) => {
   return (
     <div>
       <h3>Current Party-ers:</h3>
-      
+
       {users.map(user => <div key={user.id}>{user.name} {
-        
-        flag && username !== user.name ? user.character : null 
-        
+
+        flag && username !== user.name ? user.character : null
+
       } </div>)}
+
+      <a href="https://icebreakerideas.com/who-am-i-game/" target="_blank"><h3>Game Rules:</h3></a>
+      <p> Ask questions to guess your character!</p>
 
       <button type="button" onClick={showCharacter}>Play Game</button>
 
