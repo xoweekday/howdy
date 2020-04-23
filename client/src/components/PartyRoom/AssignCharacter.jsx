d993d05070460bc6a27053f4bacaf5829407ef23
hartsean <hartsean@gmail.com>
2020-04-22 21:28:44 -0500

    (refactor) moved randomCharacter funciton to backend

--- client/src/components/PartyRoom/AssignCharacter.jsx
+++ client/src/components/PartyRoom/AssignCharacter.jsx
@@ -1,71 +1,10 @@
 import React from 'react';
 
 const AssignCharacter = ({ users }) => {
-
-  let characters = [
-    'Snow White',
-    'Snoopy',
-    'Scooby Doo',
-    'John Wayne',
-    'Anne Hathaway',
-    'Duke Ellington',
-    'Madonna',
-    'Superman',
-    'Batman',
-    'Robin',
-    'George Washington',
-    'Abraham Lincoln',
-    'Thomas Edison',
-    'Benjamin Franklin',
-    'Brittany Spears',
-    'Cinderella',
-    'Sleeping Beauty',
-    'Billy Joel',
-    'Albert Einstein',
-    'Richard Nixon',
-    'Arnold Schwarzenegger',
-    'Dora the Explorer',
-    'Elmo',
-    'Howard Stern',
-    'Donald Trump',
-    'Rosie O’Donnell',
-    'Oprah Winfrey',
-    'Helen of Troy',
-    'Helen Keller',
-    'Cleopatra',
-    'Queen Elizabeth',
-    'Demi Moore',
-    'Angelina Jolie',
-    'Bill Clinton',
-    'Hillary Clinton',
-    'George Clooney',
-    'Rachael Ray',
-    'Martha Stewart',
-    'Magic Johnson',
-    'Dennis Miller',
-    'Michael Jackson',
-    'Brad Pitt',
-    'John Lennon',
-    'Elvis',
-    'Tom Sawyer',
-    'Napoleon',
-    'Cleopatra',
-    'Joan of Arc',
-    'SpongeBob',
-    'Ellen DeGeneres',
-    'Simon Cowell',
-    'George Bush'
-  ];
-
-  const getRandomCharacter = (characters) => {
-    let index = Math.floor(Math.random() * characters.length);
-    return characters[index];
-  }
-
+  
   function handleClick(e) {
     e.preventDefault();
-    let character = getRandomCharacter(characters);
-    console.log(character);
+    users.forEach(user=> console.log(user.character));
   }
 
   return (
