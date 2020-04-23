8669be38e4435c94247d2f7b02223041cd6b06d6
hartsean <hartsean@gmail.com>
2020-04-22 21:31:23 -0500

    (feat)added getRandomCharacter funciton and assisgned character to user

--- server/users.js
+++ server/users.js
@@ -1,10 +1,70 @@
 const users = [];
 
+let characters = [
+  'Snow White',
+  'Snoopy',
+  'Scooby Doo',
+  'John Wayne',
+  'Anne Hathaway',
+  'Duke Ellington',
+  'Madonna',
+  'Superman',
+  'Batman',
+  'Robin',
+  'George Washington',
+  'Abraham Lincoln',
+  'Thomas Edison',
+  'Benjamin Franklin',
+  'Brittany Spears',
+  'Cinderella',
+  'Sleeping Beauty',
+  'Billy Joel',
+  'Albert Einstein',
+  'Richard Nixon',
+  'Arnold Schwarzenegger',
+  'Dora the Explorer',
+  'Elmo',
+  'Howard Stern',
+  'Donald Trump',
+  'Rosie O’Donnell',
+  'Oprah Winfrey',
+  'Helen of Troy',
+  'Helen Keller',
+  'Cleopatra',
+  'Queen Elizabeth',
+  'Demi Moore',
+  'Angelina Jolie',
+  'Bill Clinton',
+  'Hillary Clinton',
+  'George Clooney',
+  'Rachael Ray',
+  'Martha Stewart',
+  'Magic Johnson',
+  'Dennis Miller',
+  'Michael Jackson',
+  'Brad Pitt',
+  'John Lennon',
+  'Elvis',
+  'Tom Sawyer',
+  'Napoleon',
+  'Cleopatra',
+  'Joan of Arc',
+  'SpongeBob',
+  'Ellen DeGeneres',
+  'Simon Cowell',
+  'George Bush'
+];
+
+const getRandomCharacter = (characters) => {
+  let index = Math.floor(Math.random() * characters.length);
+  return characters[index];
+}
+
 const addUser = ({ id, name, room }) => {
   const user = { id, name, room };
-
+  //Add a random character alias for gaming purposes
+  user.character = getRandomCharacter(characters);
   users.push(user);
-
   return user;
 }
 
