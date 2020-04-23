272728bf8e55075d121012c366603c59a1ab336d
matthritz <atmatthritz@gmail.com>
2020-04-23 12:17:40 -0500

    latest push

--- client/src/components/PartyRoom/ChatMessage.jsx
+++ client/src/components/PartyRoom/ChatMessage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ChatMessage = ({ messages }) => (
-  <div className="message">
+  <div className="messages">
     {messages.map((message, i) => <div key={i}>{message.user}: {message.text}</div>)}
   </div>
 )
