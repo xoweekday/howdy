34eee49a524a1d0385601f9d3a3ab20cd13c1448
James Easter <james.m.easter@gmail.com>
2020-04-23 14:38:30 -0500

    (feat) add and format date in party list

--- client/src/components/PartyCreation/PartyListItem.jsx
+++ client/src/components/PartyCreation/PartyListItem.jsx
@@ -36,8 +36,9 @@ const PartyListItem = ({ party, getPartyInfo, longitude, latitude }) => {
       <div className="party-container container-fluid border">
         <div className="row party-list-item">
           <div className="partyName col">{party.name}</div>
-          <div className="partyStart col"><Moment parse="HH:mm:ss" format="hh:mm a" >{party.start}</Moment></div>
-          <div className="partyEnd col"><Moment parse="HH:mm:ss" format="hh:mm a" >{party.end}</Moment></div>
+          <div className="partyName col"><Moment parse="YYYY-MM-DD" format="MMM D, YYYY" >{party.date}</Moment></div>
+          <div className="partyStart col"><Moment parse="HH:mm:ss" format="h:mm a" >{party.start}</Moment></div>
+          <div className="partyEnd col"><Moment parse="HH:mm:ss" format="h:mm a" >{party.end}</Moment></div>
           <div className="partyEnd col">{party.city}</div>
           <div className="partyEnd col">{party.radius}</div>
           <div className="partyDetails col" >{party.details}</div>
