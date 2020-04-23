19639a51cafc13e9f8361593886806c075acb33a
hartsean <hartsean@gmail.com>
2020-04-22 22:03:49 -0500

    Removed Components

--- client/src/components/PartyRoom/ChatSidebar.jsx
+++ client/src/components/PartyRoom/ChatSidebar.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ChatPeopleList from './ChatPeopleList.jsx';
 import ChatDetails from './ChatDetails.jsx';
-import AssignCharacter from './AssignCharacter.jsx';
-import GameRules from './GameRules.jsx';
 
 const ChatSidebar = ({ users, partyInfo, username }) => {
   return (
@@ -10,8 +8,6 @@ const ChatSidebar = ({ users, partyInfo, username }) => {
       <div className="d-flex-col">
         <ChatPeopleList users={users} username={username} />
         <ChatDetails partyInfo={partyInfo} />
-        <GameRules />
-        <AssignCharacter users={users} />
       </div>
     </div>
   );
