da9644379ae6ccfefc410366ccbf3db78044b5bf
chryen <squirtlexomg@yahoo.com>
2020-04-24 10:42:30 -0500

    (refactor) Removes the from title

--- client/src/components/PartyRoom/ChatHeader.jsx
+++ client/src/components/PartyRoom/ChatHeader.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ChatHeader = ({ partyInfo }) => (
   <div className="container">
-    <h3>Welcome to the {partyInfo.name}</h3>
+    <h3>Welcome to {partyInfo.name}</h3>
   </div>
 )
 
