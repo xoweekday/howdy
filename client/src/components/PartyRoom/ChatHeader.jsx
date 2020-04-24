24d843e723b875d2943798c9e41bbbf4bae928a2
chryen <squirtlexomg@yahoo.com>
2020-04-24 18:03:04 -0500

    (cleanup) eslint - fixed expression to have space

--- client/src/components/PartyRoom/ChatHeader.jsx
+++ client/src/components/PartyRoom/ChatHeader.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 const ChatHeader = ({ partyInfo }) => (
   <div className="container">
     <h3>
-      Welcome to
-      {partyInfo.name}
+      Welcome to {partyInfo.name}
     </h3>
   </div>
 );
