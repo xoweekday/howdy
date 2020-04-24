2c1453bfa3fc748c63db4c47e9f896da19eaeff5
chryen <squirtlexomg@yahoo.com>
2020-04-24 16:55:50 -0500

    (cleanup) eslint - fixed indentation and destructured state for render

--- client/src/components/PartyCreation/CreateParty.jsx
+++ client/src/components/PartyCreation/CreateParty.jsx
@@ -61,87 +61,95 @@ class CreateParty extends React.Component {
   }
 
   render() {
+    const {
+      name,
+      details,
+      start,
+      end,
+      date,
+      radius,
+    } = this.state;
     return (
       <div className="party-creation" >
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col center">
-            <form onSubmit={this.handleSubmit}>
-              <h4>Host a Party</h4>
-              <div className="form-group creation-group">
-                <label>Party</label>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col center">
+              <form onSubmit={this.handleSubmit}>
+                <h4>Host a Party</h4>
+                <div className="form-group creation-group">
+                  <label>Party</label>
                   <input
                     className="form-control creation-items"
                     name="name"
                     type="text"
-                    value={this.state.name}
+                    value={name}
                     placeholder="Name Your Party"
                     onChange={this.handleChange}
-                    />
-              </div>
-              <div className="form-group creation-group">
-            <label>Party Details</label>
-            <textarea
-              className="form-control creation-items"
-              name="details"
-              rows="3.5"
-              placeholder="80's theme? Signature cocktail? Let your neighbors know!"
-              value={this.state.details}
-              onChange={this.handleChange}
-              />
-              </div>
-              <div className="form-group creation-group">
-                <label>Start Time</label>
+                  />
+                </div>
+                <div className="form-group creation-group">
+                  <label>Party Details</label>
+                  <textarea
+                    className="form-control creation-items"
+                    name="details"
+                    rows="3.5"
+                    placeholder="80's theme? Signature cocktail? Let your neighbors know!"
+                    value={details}
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <div className="form-group creation-group">
+                  <label>Start Time</label>
                   <input
                     className="form-control creation-items"
                     name="start"
                     type="time"
-                    value={this.state.start}
+                    value={start}
                     onChange={this.handleChange}
-                    />
-              </div>
-              <div className="form-group creation-group">
-                <label>End Time</label>
+                  />
+                </div>
+                <div className="form-group creation-group">
+                  <label>End Time</label>
                   <input
                     className="form-control creation-items"
                     type="time"
                     name="end"
-                    value={this.state.end}
+                    value={end}
                     onChange={this.handleChange}
-                    />
-              </div>
-              <div className="form-group creation-group">
-                <label>Date</label>
+                  />
+                </div>
+                <div className="form-group creation-group">
+                  <label>Date</label>
                   <input
                     className="form-control creation-items"
                     type="date"
                     name="date"
-                    value={this.state.date}
+                    value={date}
                     onChange={this.handleChange}
-                    />
-              </div>
-              <div className="form-group creation-group">
-                <label>Radius</label>
+                  />
+                </div>
+                <div className="form-group creation-group">
+                  <label>Radius</label>
                   <input
                     className="form-control creation-items"
                     type="number"
                     name="radius"
                     placeholder="Miles"
-                    value={this.state.radius}
+                    value={radius}
                     onChange={this.handleChange}
-                    />
-              </div>
-              <div>
+                  />
+                </div>
+                <div>
                   <input
                     className="btn btn-primary create-submit"
                     type="submit"
                     value="submit"
-                    />
-              </div>
-            </form>
+                  />
+                </div>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
       </div>
     );
   }
