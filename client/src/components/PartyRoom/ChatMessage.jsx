4a0dc6d7e664114dcd9d87e13a8eb5fd927c08bf
chryen <squirtlexomg@yahoo.com>
2020-04-24 18:28:50 -0500

    (cleanup) eslint - fixed indentation and removed else statement

--- client/src/components/PartyRoom/ChatMessage.jsx
+++ client/src/components/PartyRoom/ChatMessage.jsx
@@ -4,23 +4,9 @@ const ChatMessage = ({ messages }) => (
   <div className="messages">
     {messages.map((message, i) => {
       if (message.text) {
-        return (
-          <div key={i}>
-            {message.user}
-            :
-            {' '}
-            {message.text}
-          </div>
-        );
+        return <div key={i}>{message.user}: {message.text}</div>;
       }
-      return (
-        <div key={i}>
-          {message.user}
-          :
-          {' '}
-          <img className="img-fluid" style={{ width: '30%' }} src={message.img} alt="" />
-        </div>
-      );
+      return <div key={i}>{message.user}: <img className="img-fluid" style={{ width: '30%' }} src={message.img} alt="" /></div>;
     })}
   </div>
 );
