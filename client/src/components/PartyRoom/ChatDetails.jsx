69d79f5920503a7af631600ab692761b6c50a1ed
chryen <squirtlexomg@yahoo.com>
2020-04-24 17:51:56 -0500

    (cleanup) eslint - fixed spacing

--- client/src/components/PartyRoom/ChatDetails.jsx
+++ client/src/components/PartyRoom/ChatDetails.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 
-const ChatDetails = ({ partyInfo }) => {
-  return (
-    <div className="container party-details">
-      <h5>Details:</h5>
-      <div className="details">
-          {partyInfo.details}
-      </div>
+const ChatDetails = ({ partyInfo }) => (
+  <div className="container party-details">
+    <h5>Details:</h5>
+    <div className="details">
+      {partyInfo.details}
     </div>
-  );
-}
+  </div>
+);
 
 export default ChatDetails;
