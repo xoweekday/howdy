efad3591e55fe53a21cd29bd8c16667842966d3f
matthritz <atmatthritz@gmail.com>
2020-04-24 16:15:30 -0500

    (refactor)shrunk header

--- client/src/components/PartyRoom/ChatDetails.jsx
+++ client/src/components/PartyRoom/ChatDetails.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ChatDetails = ({ partyInfo }) => {
   return (
     <div className="container party-details">
-      <h4>Details:</h4>
+      <h5>Details:</h5>
       <div className="details">
           {partyInfo.details}
       </div>
