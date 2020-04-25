51137822760d605698081d81d335684b3aadc230
chryen <squirtlexomg@yahoo.com>
2020-04-24 20:18:00 -0500

    (cleanup) eslint - changed to const and fixed spacing at end

--- server/game.js
+++ server/game.js
@@ -1,18 +1,16 @@
 const checkAnswer = (text, character) => {
-  let answer = text.toLowerCase().replace(/\?|\.|\,|\!/g, ' ').split(' ');
-  let names = character.toLowerCase().split(' ');
+  const answer = text.toLowerCase().replace(/\?|\.|\,|\!/g, ' ').split(' ');
+  const names = character.toLowerCase().split(' ');
   let guessedAnswer = true;
 
-  names.forEach(name => {
+  names.forEach((name) => {
     if (answer.includes(name) && guessedAnswer) {
       guessedAnswer = true;
     } else {
       guessedAnswer = false;
     }
   });
-  
   return guessedAnswer;
-}
+};
 
 module.exports = { checkAnswer };
-
