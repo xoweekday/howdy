947c1baa24a110443d1565ba04809051ab87a029
chryen <squirtlexomg@yahoo.com>
2020-04-24 19:53:45 -0500

    (cleanup) eslint - changed double to single quotes, let to const and new line for destructured exports

--- client/src/Utils/time.js
+++ client/src/Utils/time.js
@@ -6,18 +6,18 @@ const thirtyMinBeforeTodaysParty = (date, start) => {
   const now = moment();
 
   // party's start time and date
-  const startTime = moment(start, "HH:mm:ss").subtract(30, 'minutes');
+  const startTime = moment(start, 'HH:mm:ss').subtract(30, 'minutes');
   const partyDate = moment(date);
 
   // Can join 30 minutes before start on day of the party
-  return now >= startTime && partyDate.format("MMM DD, YYYY") === now.format("MMM DD, YYYY");
-}
+  return now >= startTime && partyDate.format('MMM DD, YYYY') === now.format('MMM DD, YYYY');
+};
 
 
 const currentPartiesSorted = (parties) => {
-  //create date obj from date field
-  parties.forEach(obj => {
-    let date = new Date(obj.date.substring(0,10));
+  // create date obj from date field
+  parties.forEach((obj) => {
+    const date = new Date(obj.date.substring(0,10));
     date.setDate(date.getDate() + 1);
     obj.date = date;
     // create integer out of 24hour time to sort with
@@ -25,20 +25,22 @@ const currentPartiesSorted = (parties) => {
   });
 
   // return only the current and future parties
-  let now = new Date();
-  parties = parties.filter(party => party.date >= now);
+  const now = new Date();
+  parties = parties.filter((party) => party.date >= now);
 
   // sort parties by date then time
-  parties.sort((x, y) => { return x.date - y.date || x.startInt - y.startInt; });
+  parties.sort((x, y) => x.date - y.date || x.startInt - y.startInt);
   return parties;
-}
+};
 
 const formatTime = (time) => {
-  return moment(time, "HH:mm:ss").format("h:mm a");
-}
+  return moment(time, 'HH:mm:ss').format('h:mm a');
+};
 
 const formatDate = (date) => {
-  return moment(date).format("MMM DD, YYYY");
-}
+  return moment(date).format('MMM DD, YYYY');
+};
 
-module.exports = { thirtyMinBeforeTodaysParty, currentPartiesSorted, formatTime, formatDate };
+module.exports = {
+  thirtyMinBeforeTodaysParty, currentPartiesSorted, formatTime, formatDate,
+};
