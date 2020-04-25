25e767cd8b564c4c366452016902c646de8f1efa
James Easter <james.m.easter@gmail.com>
2020-04-25 11:12:59 -0500

    (refactor) dummy data

--- schema.sql
+++ schema.sql
@@ -58,13 +58,12 @@ INSERT INTO messages (message) VALUES ('hi');
 INSERT INTO messages (message) VALUES ('this is an example message');
 INSERT INTO users (google_id, image_url, name, latitude, longitude) VALUES (123456789, 'https://vignette.wikia.nocookie.net/kyoukainokanata/images/2/20/Mirai_Kuriyama.png/revision/latest?cb=20131009053737&path-prefix=es', 'Mirai Kuriyama', 29.961919, -90.057390);
 INSERT INTO users (google_id, image_url, name, latitude, longitude) VALUES (987654321, 'https://avatars0.githubusercontent.com/u/57510574?s=460&u=04ca64a9c9ddaa924a8ee2168b2c714d29cb02b9&v=4', 'Jess', 29.961919, -90.057390);
-INSERT INTO rooms (name, host_id, host_lat, host_long, city, region, radius, details, date, start, end) VALUES ('Pokemon Party', 1, 30.695366, -88.039894, 'Mobile', 'Alabama', 500, 'This is a detail of Pokemon Party', '2020-07-20', '00:00:00', '23:59:59');
-INSERT INTO rooms (name, host_id, host_lat, host_long, city, region, radius, details, date, start, end) VALUES ('League Party', 2, 30.471165, -91.147385, 'Baton Rouge', 'Louisiana', 50, 'This is a detail of League Party', '2020-06-20', '00:00:00', '23:59:59');
+INSERT INTO rooms (name, host_id, host_lat, host_long, city, region, radius, details, date, start, end) VALUES ('Pokemon Party', 1, 30.695366, -88.039894, 'Mobile', 'Alabama', 500, 'Gotta catch\'em all! ', '2020-07-24', '11:00:00', '14:00:00');
+INSERT INTO rooms (name, host_id, host_lat, host_long, city, region, radius, details, date, start, end) VALUES ('League Party', 2, 30.471165, -91.147385, 'Baton Rouge', 'Louisiana', 50, 'Ask Gordon?', '2020-06-12', '02:00:00', '05:00:00');
 
 -- dummy data for location restrictions
 
-INSERT INTO users (name, latitude, longitude) VALUES ('Chris', 40.730610, -73.935242);
-INSERT INTO rooms (name, host_id, host_lat, host_long, radius, city, region, date, start, end) VALUES ('Everyone Join!', 3, 40.730610, -73.935242, 1350, 'New York City', 'New York', '2020-09-20', '02:34:00', '14:15:00');
-INSERT INTO users (name, latitude, longitude) VALUES ('James', 30.009631, -90.158193);
-INSERT INTO rooms (name, host_id, host_lat, host_long, radius, city, region, date, start, end) VALUES ('Kenner Only', 4, 29.9941, -90.2417, 1, 'Kenner', 'Louisiana', '2020-11-01', '01:04:00', '18:00:00');
-INSERT INTO users (name, latitude, longitude) VALUES ('Heather', 29.959757, -90.063974);
+INSERT INTO rooms (name, host_id, host_lat, host_long, radius, details, city, region, date, start, end) VALUES ('Solidarity With NYC', 3, 40.730610, -73.935242, 1350, 'Toast to NYC with your favorite drink', 'New York City', 'New York', '2020-04-27', '08:00:00', '12:00:00');
+INSERT INTO rooms (name, host_id, host_lat, host_long, radius, details, city, region, date, start, end) VALUES ('Kenner Only', 4, 29.9941, -90.2417, 1, 'Virtual Crawfish Boil', 'Kenner', 'Louisiana', '2020-04-27', '07:00:00', '12:00:00');
+INSERT INTO rooms (name, host_id, host_lat, host_long, radius, details, city, region, date, start, end) VALUES ('Jazz-Festing at Home', 4, 29.9511, -90.0715, 30, 'Share your favorite JazzFest memories', 'New Orleans', 'Louisiana', '2020-04-25', '08:15:00', '12:00:00');
+INSERT INTO rooms (name, host_id, host_lat, host_long, radius, details, city, region, date, start, end) VALUES ('Jazz-Festing at Home', 4, 29.9511, -90.0715, 30, 'Share your favorite JazzFest memories', 'New Orleans', 'Louisiana', '2020-04-27', '08:00:00', '12:00:00');
