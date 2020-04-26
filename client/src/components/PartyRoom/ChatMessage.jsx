bb39cb581bdafc4707e6052083e610ed51e0ebcf
James Easter <james.m.easter@gmail.com>
2020-04-25 22:38:49 -0500

    (fix) add PropTypes & formatted ChatMessage

--- client/src/components/PartyRoom/ChatMessage.jsx
+++ client/src/components/PartyRoom/ChatMessage.jsx
@@ -1,14 +1,40 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const ChatMessage = ({ messages }) => (
   <div className="messages">
     {messages.map((message, i) => {
       if (message.text) {
-        return <div key={i}>{message.user}: {message.text}</div>;
+        return (
+          <div key={i}>
+            {message.user}
+            {': '}
+            {message.text}
+          </div>
+        );
       }
-      return <div key={i}>{message.user}: <img className="img-fluid" style={{ width: '30%' }} src={message.img} alt="" /></div>;
+      return (
+        <div key={i}>
+          {message.user}
+          {': '}
+          <img
+            className="img-fluid"
+            style={{ width: '30%' }}
+            src={message.img}
+            alt=""
+          />
+        </div>
+      );
     })}
   </div>
 );
+
+ChatMessage.propTypes = {
+  messages: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)),
+};
+
+ChatMessage.defaultProps = {
+  messages: [],
+};
 
 export default ChatMessage;
