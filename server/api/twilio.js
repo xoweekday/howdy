a5a7d7a368616ae73c3a4de1462ca509cf2db5a4
Kenny-QD <KennyQDang@gmail.com>
2020-04-30 17:54:14 -0500

    cron, successful text message being sent 10 mins prior

--- server/api/twilio.js
+++ server/api/twilio.js
@@ -1,6 +1,8 @@
 const { Router } = require('express');
 const twilio = require('twilio');
 require('dotenv').config();
+const cron = require('node-cron');
+const db = require('../db');
 
 // twilio requirements -- texting api
 const accoutSid = process.env.S_ID;
@@ -8,6 +10,22 @@ const authToken = process.env.AUTH_TOKEN;
 const client = new twilio(accoutSid, authToken);
 
 const twilioRoute = Router();
+
+
+cron.schedule('* * * * *', () => {
+  db.checkUpcomingParty()
+    .then((result) => {
+      result.forEach((party) => {
+        client.messages.create({
+          body: `Your party: ${party.name} is starting in 10 minutes`,
+          to: `+1${party.phone}`,
+          from: process.env.PHONE_NUMBER,
+        });
+      });
+    })
+    .catch((err) => console.log(err, 'this is our error'));
+});
+
 
 twilioRoute.post('/', (req, res) => {
   const { recipient, textmessage } = req.body;
@@ -20,6 +38,7 @@ twilioRoute.post('/', (req, res) => {
     .then((message) => res.status(200).send(message.body))
     .catch((err) => console.error(err));
 });
+
 
 module.exports = {
   twilioRoute,
