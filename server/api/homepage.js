f82e5bca090a0d6ae84182ba384f8178ae7ffa26
Kenny-QD <KennyQDang@gmail.com>
2020-04-30 18:24:38 -0500

    merged homepage

--- server/api/homepage.js
+++ server/api/homepage.js
@@ -7,17 +7,10 @@ const homeRouter = Router();
 // when a user creates a party, add the details to the database
 homeRouter.post('/', (req, res) => {
   db.addParty(req)
-<<<<<<< HEAD
-    .then(() => res.send(201))
-    .catch((err) => {
-      console.log(err);
-      res.sendStatus(500).send(err);
-=======
     .then(() => res.sendStatus(201))
     .catch((error) => {
       console.log(error);
       res.sendStatus(500);
->>>>>>> 04b574d657ea51a0d4273757a26ec7c56d3b52a8
     });
 });
 
