3282fe01190b4934f7f33fb30507af43390b30df
nicopaulino <nicopaulino8@gmail.com>
2020-04-30 12:13:32 -0500

    Change Dropdown to React Select

--- client/src/components/PartyRoom/ChatInput.jsx
+++ client/src/components/PartyRoom/ChatInput.jsx
@@ -1,43 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import Select from 'react-select';
 
-const optionChange = (user) => {
-  console.log(`You are now private messaging ${user}`);
-};
 
 const ChatInput = ({
   message, sendMessage, setMessage, users,
-}) => (
-  <div className="container-fluid">
-    <div className="row d-flex-row">
-      <div className="col-10 form-group">
-        <select className="userNames" name="users">
-          <option value="Everyone" onChange={optionChange('everyone')}>Everyone</option>
-          {users.map((user) => (
-            <option value={user.name} onChange={optionChange(user.name)}>{user.name}</option>
-          ))}
-        </select>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Chatboard: enter message"
-          value={message}
-          onChange={(event) => setMessage(event.target.value)}
-          onKeyPress={(event) => (event.key === 'Enter' ? sendMessage(event) : null)}
-        />
-      </div>
-      <div className="col-2">
-        <button
-          type="button"
-          className="btn btn-primary form-control send-button"
-          onClick={(event) => sendMessage(event)}
-        >
-          SEND
-        </button>
+}) => {
+  const [selectOptions, setOptions] = useState([]);
+  const [selected, setSelect] = useState(users[0]);
+  useEffect(() => {
+    setOptions(users.map((user) => ({
+      value: user,
+      label: user.name,
+    })));
+  }, [users]);
+  return (
+    <div className="container-fluid">
+      <div className="row d-flex-row">
+        <div className="col-10 form-group">
+          <Select
+            options={selectOptions}
+            onChange={(e) => {
+              setSelect(e.value);
+              console.log(selected);
+            }}
+          />
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Chatboard: enter message"
+            value={message}
+            onChange={(event) => setMessage(event.target.value)}
+            onKeyPress={(event) => (event.key === 'Enter' ? sendMessage(event) : null)}
+          />
+        </div>
+        <div className="col-2">
+          <button
+            type="button"
+            className="btn btn-primary form-control send-button"
+            onClick={(event) => sendMessage(event)}
+          >
+            SEND
+          </button>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 ChatInput.propTypes = {
   message: PropTypes.string,
